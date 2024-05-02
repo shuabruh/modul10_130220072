9528894d7bd3a50c71da93aa026ffa76dab18870
shuabruh <shuabruh@student.telkomuniversity.ac.id>
2024-05-02 10:24:30 +0700

    Tambahkan class Mahasiswa untuk API mahasiswa

---
+++ modul10_130220072/Controllers/MahasiswaController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace modul10_130220072.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahsiswaController : ControllerBase
+    {
+        private static List<string> courses = new List<string>
+        { "KPL", "BD", "PBO" };
+
+        private static List<Mahasiswa> semuaMahasiswa = new List<Mahasiswa>
+        {
+            new Mahasiswa("Joshua Daniel Simanjuntak", "1302220072", courses, 2022),
+            new Mahasiswa("Rakha Galih Nugraha Sukma", "1302223118", courses, 2022),
+            new Mahasiswa("Mohammed Yousef Gumilar", "1302220085", courses, 2022),
+            new Mahasiswa("Irvan Dzawin Nuha", "1302223076", courses, 2022),
+            new Mahasiswa("Abdillah Aufa Taqiyya", "1302220131", courses, 2022)
+        };
+
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get() { return semuaMahasiswa; }
+
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa newvalue)
+        {
+            semuaMahasiswa.Add(newvalue);
+        }
+
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return semuaMahasiswa[id];
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            semuaMahasiswa.RemoveAt(id);
+        }
+    }
+}
---
+++ modul10_130220072/Mahasiswa.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection.Metadata;
+
+namespace modul10_130220072
+{
+    public class Mahasiswa
+    {
+        public string nama { get; set; }
+        public string nim { get; set; }
+        public List<string> courses { get; set; }
+        public int year { get; set; }
+
+        public Mahasiswa(string nama, string nim, List<string> courses, int year)
+        {
+            this.nama = nama;
+            this.nim = nim;
+            this.courses = courses;
+            this.year = year;
+        }
+    }
+}
